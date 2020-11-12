2d237515e563f9a0c5051e7776e5de3419f3c736
sungcoders <levansung28@gmail.com>
2020-11-12 22:39:03 +0700

    gui lenh at tu arduino qua esp8266v1 thanh cong

---
+++ AT_SEND_TO_ESP8266V1_FROM_ARDUINO/AT_SEND_TO_ESP8266V1_FROM_ARDUINO/AT_SEND_TO_ESP8266V1_FROM_ARDUINO.ino
@@ -0,0 +1,23 @@
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial2.begin(115200);
+  uart_send("AT\r\n",5000);
+  uart_send("AT+GMR\r\n",5000);
+  uart_send("AT+RST\r\n",5000);
+}
+
+void loop()
+{
+  
+}
+
+char uart_send(String str,int interval)
+{
+  char cvc[200];
+  str.toCharArray(cvc,str.length()+1);
+  Serial2.write(cvc);
+  delay(interval);
+  return (cvc);
+}
